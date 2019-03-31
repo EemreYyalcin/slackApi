b9c65b645d2db909dca426f34d623146022caa7d
eemreyyalcin <eemreyyalcin@gmail.com>
2019-03-31 16:04:38 +0300

    added server connection

---
+++ src/main/resources/commands.sh
@@ -0,0 +1,8 @@
+mvn clean package; scp target/backend-2.1.1.RELEASE.jar ubuntu@ec2-18-194-181-83.eu-central-1.compute.amazonaws.com:/home/ubuntu/jar/ ;ssh ubuntu@ec2-18-194-181-83.eu-central-1.compute.amazonaws.com "killall java; java -jar /home/ubuntu/jar/backend-2.1.1.RELEASE.jar"
+
+
+# https://slack.com/oauth/authorize?client_id=571141609170.571635956869&scope=channels:history,groups:history,bot,users:read,im:history,im:read,channels:read,groups:read,users.profile:read&state=I0axGKvuz0EKLBb6zoYq
+
+
+
+
